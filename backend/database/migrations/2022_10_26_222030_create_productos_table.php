d102fe9be845f49b199926dd9fa384c5d8a25989
MayteMejia <maytemejia2504@gmail.com>
2022-10-26 19:50:18 -0400

    "registro datos productos"

---
+++ backend/database/migrations/2022_10_26_222030_create_productos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->integer('precio');
+            $table->date('fechaElaboracion');
+            $table->date('fechaVencimiento');
+            $table->date('fechaOferta');
+            $table->integer('stok');
+            $table->string('imagen');
+            $table->timestamps();
+
+            $table->foreignId('id_categoria')
+                  ->nullable()
+                  ->constrained('categorias')
+                  ->cascadeOnUpdate()
+                  ->nullOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
