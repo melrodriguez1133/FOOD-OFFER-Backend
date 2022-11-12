473d1eed45e46ba7ab772ac01bb6569fb7ed9c6c
melrodriguez1133 <202000231@est.umss.edu>
2022-11-12 14:29:42 -0400

    "modificacion registrar empresa"

--- backend/app/Http/Controllers/registrarEmpresasController.php
+++ backend/app/Http/Controllers/registrarEmpresasController.php
@@ -48,7 +48,7 @@ class registrarEmpresasController extends Controller
         $registrarEmpresas->correoEmpresa = $request->correoEmpresa;
 
         $registrarEmpresas->save();
-
+return[];
     }
 
     /**
