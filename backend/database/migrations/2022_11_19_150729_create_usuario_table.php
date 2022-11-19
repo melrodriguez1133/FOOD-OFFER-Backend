fd1d723c3c1271041953230e87bf7695f4938d9b
bruno vasquez <brunojoel444@gmail.com>
2022-11-19 11:35:43 -0400

    "Creación de tabla usuario"

---
+++ backend/database/migrations/2022_11_19_150729_create_usuario_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('edad');
+            $table->string('direccion');
+            $table->integer('celular');
+            $table->string('genero');
+            $table->integer('ci');
+            $table->string('expedido');
+            $table->string('email');
+            $table->string('contraseña');
+            $table->string('verificar_Contraseña');
+            $table->string('rol');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+};
